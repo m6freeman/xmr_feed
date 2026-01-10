0899a92dca7e6f1e16483c9dc2c63b19c73f7260
Matt Freeman <m6freeman@tuta.io>
2026-01-10 13:50:55 -0500

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let support_xmr_url: String =
         format!("https://www.supportxmr.com/api/miner/{public_address}/stats");
     let support_xmr_response: support_xmr::support_xmr::SupportXMRStatus =
-        support_xmr::support_xmr::get_status(support_xmr_url)?;
+        match support_xmr::support_xmr::get_status(support_xmr_url) {
+            Ok(value) => value,
+            Err(e) => {
+                println!("{}", e);
+                return Ok(());
+            }
+        };
 
     let coin_gecko_url: String =
         format!("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd");
     let coin_gecko_response: coin_gecko::coin_gecko::CoinGeckoResponse =
-        coin_gecko::coin_gecko::get_to_usd(coin_gecko_url)?;
+        match coin_gecko::coin_gecko::get_to_usd(coin_gecko_url) {
+            Ok(value) => value,
+            Err(e) => {
+                println!("{}", e);
+                return Ok(());
+            }
+        };
 
     println!(
         "{} {}kh/s {:.5}/{:.5} ${:.2}",
